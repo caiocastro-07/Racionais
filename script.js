8b415f9df866d93596b986a72c915174d576b795
caiocastro-07 <caiocastro.git@gmail.com>
2026-08-10 22:27:24 -0300

    resolvendo o bug do cabeçalho

---
+++ script.js
@@ -0,0 +1,11 @@
+// Correção 1: getElementById
+
+function passar_por_cima(elemento) {
+    elemento.style.backgroundColor = 'transparent'; // Muda ao passar
+  
+}   
+
+function sair_do_nav(elemento) {
+    elemento.style.backgroundColor = 'red';
+    
+}
