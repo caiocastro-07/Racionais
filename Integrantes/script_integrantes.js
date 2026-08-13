5d74f05a3cacd0bb6344c9131c4a5c6403a1250f
caiocastro-07 <caiocastro.git@gmail.com>
2026-08-13 11:01:12 -0300

    Meu Deus pq não ta indo

---
+++ Integrantes/script_integrantes.js
@@ -0,0 +1,11 @@
+// Correção 1: getElementById
+
+function passar_por_cima(elemento) {
+    elemento.style.backgroundColor = 'darkred'; // Muda ao passar
+  
+}   
+
+function passar_do_nav(elemento) {
+    elemento.style.backgroundColor = 'transparent';
+    
+}
